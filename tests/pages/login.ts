416fba1672cd76433098823ce7c390e667bd8d0a
Sidrah Ihsan <146080227+sihsan@users.noreply.github.com>
2025-06-21 12:54:15 +0500

    updated test for the employee

--- tests/pages/login.ts
+++ tests/pages/login.ts
@@ -2,6 +2,7 @@ import { click, fill } from '@utils/action-utils';
 import { getLocatorByPlaceholder, getLocatorByRole, getLocatorByText } from '@utils/locator-utils';
 import { gotoURL } from '@utils/page-utils';
 import { expectElementToBeVisible, expectPageToHaveURL } from '@utils/assert-utils';
+import { invalidCredentials, validCredentials } from '@testdata/userInfo';
 
 const userName = () => getLocatorByPlaceholder('Username');
 const password = () => getLocatorByPlaceholder('Password');
@@ -12,16 +13,16 @@ export async function navigateToTheLoginPage() {
   await gotoURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 }
 
-export async function loginWithValidCreds() {
-  await fill(userName(), 'Admin');
-  await fill(password(), 'admin123');
+export async function loginWithValidCreds(credentials = validCredentials) {
+  await fill(userName(), credentials.username);
+  await fill(password(), credentials.password);
   await click(loginButton());
   await expectPageToHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
 }
 
-export async function loginWithInvalidCreds() {
-  await fill(userName(), 'invalidUser');
-  await fill(password(), 'invalidPassword');
+export async function loginWithInvalidCreds(credentials = invalidCredentials) {
+  await fill(userName(), credentials.username);
+  await fill(password(), credentials.password);
   await click(loginButton());
   await expectElementToBeVisible(
     errorMessageForInvalidCreds(),
